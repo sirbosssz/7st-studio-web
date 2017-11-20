0c99cd93f845d642e2d27dae2dae6814eb9ea5f8
Sanhaphat Srijumpa <fluke031@hotmail.com>
2017-11-20 22:08:47 +0700

    update index animation

--- animation.js
+++ animation.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
+  indexAnimation();
+});
+
+$(function indexAnimation(){
   $('#index').animate({'left' : '0', 'opacity' : '1'}, 500, 'swing');
   setTimeout(function(){$('.texta1').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1400);
   setTimeout(function(){$('.texta2').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1500);
-});
+})
