66fdd022345eda9d4c451e1230dec265fa81f5fd
Sanhaphat Srijumpa <fluke031@hotmail.com>
2017-11-21 00:15:48 +0700

    add animation too team page

--- animation.js
+++ animation.js
@@ -1,6 +1,6 @@
 var $lastId;
 
-$(document).ready(function () {
+$(document).ready(function(){
   indexAnimation();
 });
 
@@ -31,8 +31,8 @@ $("#nav-button-contact").click(function(){
 
 function indexAnimation(){
   $('#index').animate({'opacity' : '1'}, 500, 'swing');
-  setTimeout(function(){$('.texta1').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1400);
-  setTimeout(function(){$('.texta2').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1500);
+  setTimeout(function(){$('.texta1').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1200);
+  setTimeout(function(){$('.texta2').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1400);
   $lastId = "#"+$('#index').attr('id');
 }
 
@@ -48,6 +48,12 @@ function galleryAnimation(){
 
 function teamAnimation(){
   $('#team').animate({'opacity' : '1'}, 500, 'swing');
+  setTimeout(function(){$('.texta3').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1000);
+  setTimeout(function(){$('.texta4').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1200);
+  setTimeout(function(){$('.pic1').animate({'bottom' : '0', 'opacity' : '1'}, 500, 'swing');}, 2000);
+  setTimeout(function(){$('.pic2').animate({'bottom' : '0', 'opacity' : '1'}, 500, 'swing');}, 2200);
+  setTimeout(function(){$('.pic3').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 2600);
+  setTimeout(function(){$('.pic4').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 2400);
   $lastId = "#"+$('#team').attr('id');
 }
 
@@ -60,6 +66,14 @@ function resetAnimation(){
   if($lastId == "#index"){
     $('.texta1').css({'top' : '45px', 'opacity' : '0'});
     $('.texta2').css({'top' : '45px', 'opacity' : '0'});
+  }
+  else if($lastId == "#team"){
+    $('.texta3').css({'top' : '45px', 'opacity' : '0'});
+    $('.texta4').css({'top' : '45px', 'opacity' : '0'});
+    $('.pic1').css({'bottom' : '45px', 'opacity' : '0'});
+    $('.pic2').css({'bottom' : '45px', 'opacity' : '0'});
+    $('.pic3').css({'top' : '45px', 'opacity' : '0'});
+    $('.pic4').css({'top' : '45px', 'opacity' : '0'});
   }
   $($lastId).css({'opacity' : '0'});
 }
