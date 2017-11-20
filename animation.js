418819051c2e27f7cf543e41e4c403978d7d21f8
Sanhaphat Srijumpa <fluke031@hotmail.com>
2017-11-20 22:05:20 +0700

    add animation on index

---
+++ animation.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $('#index').animate({'left' : '0', 'opacity' : '1'}, 500, 'swing');
+  setTimeout(function(){$('.texta1').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1400);
+  setTimeout(function(){$('.texta2').animate({'top' : '0', 'opacity' : '1'}, 500, 'swing');}, 1500);
+});
